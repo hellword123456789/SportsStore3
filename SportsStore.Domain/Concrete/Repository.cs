a02346d5a607eb4172d956d4a13696c316d16ef7
114515704 <114515704@qq.com>
2018-10-20 14:29:52 +0800

    11

---
+++ SportsStore.Domain/Concrete/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class Repository
+    {
+        //实例化EF容器:有弊端。一个线程里可能会创建多个DbContext
+        //DbContext db = new DbContext();
+
+        //改造：保证一个请求线程中只有一份EF容器（你要明白：一个url请求到服务器，IIS就开一个线程去处理）
+        public static EFDbContext GetDbContext
+        {
+            get
+            {
+                //向线程缓存中查询，如果返回的是null，则创建，同时存入到这个线程缓存中
+                //注意的是线程缓存CallContext，而不是我们熟悉的HttpRuntime.cache。意味着这个DbContext对象在这个线程内能被其他方法共享。
+                object efDbContext = CallContext.GetData("EFDbContext");
+                if (efDbContext == null)
+                {
+                    efDbContext = new EFDbContext();
+                    //存入到这个线程缓存中
+                    CallContext.SetData("EFDbContext", efDbContext);
+                }
+                return efDbContext as EFDbContext;
+            }
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+       
+        private IProductRepository repository;
+        int pageSize = 1;
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+        
+        // GET: /Product/
+        public ActionResult List(string category, int page=1)
+        {
+            
+            IEnumerable<Product> prods = repository.Products
+                .Where(x => category == null || x.Category == category);
+            IEnumerable<Product> prod = prods
+                .OrderBy(k => k.ProductID).Skip((page - 1) * pageSize).Take(pageSize);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = prod,
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = prods.Count()
+                },
+                CurrentCategory=category
+            };
+            
+            return View(model);
+        }
+	}
+}
